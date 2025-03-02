226e867decd6b1904eaab0120229fff34225b30c
Vladimir Beliakov <ermegilius@gmail.com>
2025-03-02 14:25:02 +0200

    style: give cards a hover effect

--- frontend/src/components/CountryCard.tsx
+++ frontend/src/components/CountryCard.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { Country } from "../types/country";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 
 interface CountryCardProps {
 	country: Country;
@@ -19,29 +18,39 @@ const CountryCard = ({ country }: CountryCardProps) => {
 	};
 
 	return (
-		<Card sx={{ borderRadius: 0 }}>
-			<CardMedia
-				component="img"
-				height="140"
-				image={country.flags.png}
-				alt={country.flags.alt || country.name.common}
-			/>
-			<CardContent>
-				<Typography variant="h5" component="div">
-					{country.name.common}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					Region: {country.region}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					Capital: {country.capital ? country.capital.join(", ") : "N/A"}
-				</Typography>
-			</CardContent>
-			<CardActions>
-				<Button size="small" onClick={handleDetailsClick}>
-					Details
-				</Button>
-			</CardActions>
+		<Card
+			variant="outlined"
+			sx={{
+				borderRadius: 0,
+				borderBottomLeftRadius: 4,
+				borderBottomRightRadius: 4,
+				maxWidth: 345,
+				transition: "background-color 0.2s",
+				"&:hover": {
+					backgroundColor: (theme) => theme.palette.action.hover,
+				},
+			}}
+			onClick={handleDetailsClick}
+		>
+			<CardActionArea>
+				<CardMedia
+					component="img"
+					height="140"
+					image={country.flags.png}
+					alt={country.flags.alt || country.name.common}
+				/>
+				<CardContent>
+					<Typography variant="h5" component="div">
+						{country.name.common}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Region: {country.region}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Capital: {country.capital ? country.capital.join(", ") : "N/A"}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
 		</Card>
 	);
 };
